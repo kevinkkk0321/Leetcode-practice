6eb24c6fa71e119438e66a4c4f4a7204815d33f8
kevinkkk08 <chiukevin08@gmail.com>
2018-05-06 23:27:46 +0800

    2018/5/6

--- 1. Two sum.c
+++ 1. Two sum.c
@@ -1,6 +1,43 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ 
+ /*Hash table*/
+ int* twoSum(int* nums, int numsSize, int target) {  
+    int i = 0;  
+    int min;  
+    min = nums[0];  
+    for (i = 0; i < numsSize; i++) {  
+       if (nums[i] < min)  
+           min = nums[i];  
+    }  
+    int max = target - min;  
+    int len = max - min + 1;     
+    int *table = (int*)malloc(len*sizeof(int));  
+    int *index = (int*)malloc(2*sizeof(int));  
+    if (table == NULL) {  
+        exit(1);  
+    }  
+    for (i = 0; i < len; i++) {  
+        table[i] = -1;  
+    }  
+    for (i = 0; i < numsSize; i++) {  
+        if (nums[i] <= max) {  
+            if (table[target - nums[i] -min] != -1) {  
+                index[0] = table[target - nums[i] -min];  
+                index[1] = i;  
+                break;  
+            }  
+            table[nums[i] -min] = i;  
+        }  
+    }  
+    free(table);  
+    return index;  
+}  
+ 
+ 
+ 
+ /* Iterative*/
 int* twoSum(int* nums, int numsSize, int target) {
     int* array = (int *)malloc(sizeof(int)*3);
     for(int i=0;i<numsSize;++i){
---
+++ 242. Valid Anagram.c
@@ -0,0 +1,34 @@
+/* C -- method 1 */
+bool isAnagram(char* s, char* t) {
+    int a[26]={0},b[26]={0};  
+    int i;  
+    if(strlen(s)!=strlen(t))  
+        return false;  
+    i=0;  
+    while(s[i]!='\0')  
+    {  
+        a[s[i]-'a']++;  
+        b[t[i]-'a']++;  
+        i++;  
+    }  
+    for(i=0;i<26;i++)  
+    {  
+        if(a[i]!=b[i])  
+            break;  
+    }  
+    if(i==26)  
+        return true;  
+    else  
+        return false;  
+}
+
+
+/*C++ -- method 2*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());  
+        sort(t.begin(),t.end());//sort是c++中的一个排序函数。sort（s.begin(),s.end()）表示对给定区间所有元素进行排序。  
+        return s==t;  
+    }
+};
