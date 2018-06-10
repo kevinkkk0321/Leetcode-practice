4379c461dd50f5d0fccb0f93d96c782e0d5ba402
kevinkkk08 <chiukevin08@gmail.com>
2018-06-10 23:48:35 +0800

    2018/6/10
    second commitment

--- 70. Climbing Stairs.c
+++ 70. Climbing Stairs.c
@@ -1,28 +1,41 @@
 int table[500];
 
-void dp(){
+
+void method_one(){
     table[1]=1;
     table[2]=2;
-    //table[2]=2;
     for(int i=3;i<500;i++){
         table[i] = table[i-1]+ table[i-2];
     }
 }
 
-int record(int n){
+int method_two(int n){
     if(n==1 ) return 1;
-    else if(n==2 ) return 2;
-    
-    if(table[n]) return table[n];
-    
+    else if(n==2 ) return 2;    
+    if(table[n]) return table[n];    
     return table[n] = climbStairs(n-1)+climbStairs(n-2);
 }
 
+int method_three(int n){
+    int array[2]={1,1};
+    for(int i=1;i<n;i++){
+        int temp=array[1];
+        array[1]+=array[0];
+        array[0]=temp;
+    }
+    return array[1];  
+}
+
+
+
 int climbStairs(int n) {
     /*method 1*/
-    dp();
-    return table[n];
+    //method_one();
+    //return table[n];
     
     /*method 2*/
-    //return record(n);
+    //return method_two(n);
+    
+    /*method 3*/
+    return method_three(n);
 }
