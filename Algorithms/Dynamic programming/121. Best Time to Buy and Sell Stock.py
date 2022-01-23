8652300e755f0eb496fd8fb51013bbb7ed961dc6
Kevin Chiu <chiukevin08@gmail.com>
2022-01-24 00:21:44 +0800

    20220123_sellstock

--- Algorithms/Dynamic programming/121. Best Time to Buy and Sell Stock.py
+++ Algorithms/Dynamic programming/121. Best Time to Buy and Sell Stock.py
@@ -4,15 +4,27 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices) == 1:
+        if len(prices) <= 1:
             return 0
-        
-        maxprice = [0]*len(prices)
-        cur_price = 0
-        max_result = 0
-        
-        for i in range(1, len(prices)):
-            maxprice[i] = max(maxprice[i-1] + prices[i] - prices[i-1], 0)
-            max_result = max(max_result, maxprice[i])
-        return max_result            
+
+        l = len(prices)
+        res = 0
+        min_price = float('inf')
+
+        for i in range(0, l):
+            min_price = min(prices[i], min_price)
+            res = max(prices[i]-min_price, res)
+        return res
+
+
+#         if len(prices) == 1:
+#             return 0
+
+#         maxprice = [0]*len(prices)
+#         max_result = 0
+
+#         for i in range(1, len(prices)):
+#             maxprice[i] = max(maxprice[i-1] + prices[i] - prices[i-1], 0)
+#             max_result = max(max_result, maxprice[i])
+#         return max_result
             
