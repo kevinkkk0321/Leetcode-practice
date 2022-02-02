119bad122e549b539906c2d327deac852a22753f
Kevin Chiu <chiukevin08@gmail.com>
2022-02-02 20:24:07 +0800

    20220202_dp

--- Algorithms/Dynamic programming/322. Coin Change.py
+++ Algorithms/Dynamic programming/322. Coin Change.py
@@ -9,16 +9,13 @@ class Solution(object):
             return 0
         if not coins:
             return -1
-        
+
         dp = [amount+1]*(amount+1)
         dp[0] = 0
-        
+
         for i in range(1, amount+1):
             for j in range(len(coins)):
                 if coins[j]<=i:
                     dp[i] = min(dp[i], dp[i-coins[j]]+1)
-        
+
         return dp[amount] if dp[amount]<=amount else -1
-        
-        
-        
